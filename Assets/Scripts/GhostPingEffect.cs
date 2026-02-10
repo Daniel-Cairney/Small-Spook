aed6a50d97a526f4a865acb0acad3c42d7eca171
Daniel Cairney <danielcairney8@gmail.com>
2026-02-10 22:24:43 +1100

    fixed beacon bug

--- Assets/Scripts/GhostPingEffect.cs
+++ Assets/Scripts/GhostPingEffect.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class GhostPingEffect : MonoBehaviour
 {
-  
+     // this script is just a ping for the ghost beacon particles so it grabs that specific particle effect for if there are more then 1 particle effects on the prefab
 }
